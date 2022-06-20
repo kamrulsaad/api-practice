64826525eead1957926ff272b9243d153b864732
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-06-20 22:39:16 +0600

    added all products'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  console.log(products);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        products.map(pd => <div key={pd.id} className='card'>
+          <img src={pd.image} alt={pd.title} />
+          <h1>{pd.title.slice(0, 20)}</h1>
+          <p>Price: {pd.price}</p>
+          <p>Rating: {pd.rating.rate}/5 by {pd.rating.count} people</p>
+          <p>Category: {pd.category}</p>
+          <p>Description: {pd.description.slice(0, 30)}</p>
+        </div>)
+      }
     </div>
   );
 }
